fef701ddbf71394b5846f0d57d66cb1ecf66b2c6
Michael Robinson <seraph890@gmail.com>
2014-12-17 15:52:58 +1100

    reworked show method and added tests

--- grails-app/controllers/checkpoint/grails/ReconController.groovy
+++ grails-app/controllers/checkpoint/grails/ReconController.groovy
@@ -13,10 +13,10 @@ class ReconController {
         //do recon things
 
         String soapAction = "http://www.siteminder.com.au/siteconnect/HotelAvailRQ"
-        String uri = "http://localhost:6980/MathServiceInterface?wsdl"
-        String username = ""
-        String password = ""
-        String hotelCode = ""
+        String uri = params.uri.toString()
+        String username = params.username.toString()
+        String password = params.password.toString()
+        String hotelCode = params.hotelCode.toString()
 
         if(!soapAction || !uri || !username || !password) {
             println "Missing configuration options"
@@ -26,45 +26,38 @@ class ReconController {
             println "Missing hotel code"
         }
 
-        Map soapRequest = [
-            headers: headerXml(username, password),
-            payload: bodyXml(hotelCode)
-        ]
+        def request = generateXml(username, password, hotelCode)
+
+        def client = new SOAPClient(uri)
+        def response = client.send(SOAPAction: soapAction, request)
     }
 
-    private String headerXml(String username, String password) {
-        def xml = """
-            mkp.declareNamespace(soap:"http://schemas.xmlsoap.org/soap/envelope/")
-            mkp.declareNamespace(wsse:"http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd")
-            'wsse:Security'('soap:mustUnderstand': "1") {
-                'wsse:UsernameToken' {
-                    'wsse:Username'($username)
-                    'wsse:Password'(Type: "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText", $password)
-                }
-            }
-    """
-        return XmlUtil.serialize(xml as GPathResult)
-    }
-
-    private String bodyXml(String hotelCode) {
-        def echoToken = UUID.randomUUID().toString()
-        def timestamp = DateFormatUtils.ISO_DATETIME_TIME_ZONE_FORMAT.format(new Date())
-
-        def xml = """
-            mkp.declareNamespace("": "http://www.opentravel.org/OTA/2003/05")
-            OTA_HotelAvailRQ(Version: "1.0", TimeStamp: timestamp, EchoToken: $echoToken, AvailRatesOnly: "true") {
-                AvailRequestSegments {
-                    AvailRequestSegment(AvailReqType: "Room") {
-                        HotelSearchCriteria {
-                            Criterion {
-                                HotelRef(HotelCode: $hotelCode)
-                            }
-                        }
-                    }
-                }
-            }
-        """
-
-        return XmlUtil.serialize(xml as GPathResult)
+    private static String generateXml(username, password, hotelCode) {
+        def xml = """<?xml version="1.0"?>
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+    <SOAP-ENV:Header xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+        <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" soap:mustUnderstand="1">
+            <wsse:UsernameToken>
+                <wsse:Username>$username</wsse:Username>
+                <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">$password</wsse:Password>
+            </wsse:UsernameToken>
+        </wsse:Security>
+    </SOAP-ENV:Header>
+    <SOAP-ENV:Body xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+        <OTA_HotelAvailRQ xmlns="http://www.opentravel.org/OTA/2003/05" AvailRatesOnly="true" EchoToken="e01b39e8-962f-4078-ad93-8070c2e2f1a7" TimeStamp="2014-12-16T13:16:46+11:00" Version="1.0">
+            <AvailRequestSegments>
+                <AvailRequestSegment AvailReqType="Room">
+                    <HotelSearchCriteria>
+                        <Criterion>
+                            <HotelRef HotelCode="$hotelCode"/>
+                        </Criterion>
+                    </HotelSearchCriteria>
+                </AvailRequestSegment>
+            </AvailRequestSegments>
+        </OTA_HotelAvailRQ>
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+"""
+        return xml
     }
 }
--- test/unit/checkpoint/grails/ReconControllerSpec.groovy
+++ test/unit/checkpoint/grails/ReconControllerSpec.groovy
@@ -18,11 +18,47 @@ class ReconControllerSpec extends Specification {
     def cleanup() {
     }
 
-    void "test something"() {
+    void "test show should return xml response"() {
         when:
-        reconController.show()
+        params.uri = "https://193.201.124.113:4441/InvControl.asmx"
+        params.username = "site"
+        params.password = "minder"
+        params.hotelcode = "STA/STA/3"
+        def show = reconController.show()
 
         then:
-        response.text == "wont work"
+        response.text == ""
+    }
+
+    void "generateXml should generate xml with fields"() {
+        when:
+        def xml = reconController.generateXml("siteminder", "password", "12345")
+
+        then:
+        xml == """<?xml version="1.0"?>
+<SOAP-ENV:Envelope xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+    <SOAP-ENV:Header xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+        <wsse:Security xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/" soap:mustUnderstand="1">
+            <wsse:UsernameToken>
+                <wsse:Username>siteminder</wsse:Username>
+                <wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">password</wsse:Password>
+            </wsse:UsernameToken>
+        </wsse:Security>
+    </SOAP-ENV:Header>
+    <SOAP-ENV:Body xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/">
+        <OTA_HotelAvailRQ xmlns="http://www.opentravel.org/OTA/2003/05" AvailRatesOnly="true" EchoToken="e01b39e8-962f-4078-ad93-8070c2e2f1a7" TimeStamp="2014-12-16T13:16:46+11:00" Version="1.0">
+            <AvailRequestSegments>
+                <AvailRequestSegment AvailReqType="Room">
+                    <HotelSearchCriteria>
+                        <Criterion>
+                            <HotelRef HotelCode="12345"/>
+                        </Criterion>
+                    </HotelSearchCriteria>
+                </AvailRequestSegment>
+            </AvailRequestSegments>
+        </OTA_HotelAvailRQ>
+    </SOAP-ENV:Body>
+</SOAP-ENV:Envelope>
+"""
     }
 }
