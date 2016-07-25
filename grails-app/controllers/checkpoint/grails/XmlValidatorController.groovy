dd7cc6a908709a8e5055b8a9c3c0f089506dd627
Michael Robinson <seraph890@gmail.com>
2016-07-25 15:00:02 +1000

    removed unrequired code

--- grails-app/controllers/checkpoint/grails/XmlValidatorController.groovy
+++ grails-app/controllers/checkpoint/grails/XmlValidatorController.groovy
@@ -1,9 +1,6 @@
 package checkpoint.grails
 
 import checkpoint.ValidateXml
-import javax.xml.XMLConstants
-import javax.xml.transform.stream.StreamSource
-import javax.xml.validation.SchemaFactory
 
 class XmlValidatorController {
 
@@ -16,16 +13,5 @@ class XmlValidatorController {
 
 
         ValidateXml.valid(xml, xsd)
-
-        def xmlFactory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI)
-
-        try {
-          def schema = xmlFactory.newSchema(new StreamSource(new StringReader(xsd)))
-          def validator = schema.newValidator()
-
-          def errorMessage = validator.validate(new StreamSource(new StringReader(xml)))
-        } catch(e) {
-        return [xmlError: e]
-        }
     }
 }
