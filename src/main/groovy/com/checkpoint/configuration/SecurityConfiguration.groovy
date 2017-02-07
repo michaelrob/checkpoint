026cb267d86f1064fa4e6d79c62bc900a76fec80
Michael Robinson <seraph890@gmail.com>
2017-02-07 22:47:16 +0900

    added security configuration

---
+++ src/main/groovy/com/checkpoint/configuration/SecurityConfiguration.groovy
@@ -0,0 +1,13 @@
+package com.checkpoint.configuration
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity.authorizeRequests().antMatchers("/").permitAll()
+    }
+}
